a78bc49eaa2dcfa1321fa6998ab0458a7357a1b5
jduran <jduran@gen-it.com.ar>
2024-08-26 20:32:01 -0300

    create validator for login

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,19 @@
-export class CreateAuthDto {}
+import { IsEmail, IsNotEmpty, IsStrongPassword, Length } from "class-validator";
+
+export class LoginAuthDto {
+
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword({
+    minNumbers: 1,
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 0,
+    minSymbols: 0,
+  })
+  password: string;
+}
