fdb080052924e16b97ea7ce4eeb87facbf70a042
jduran <jduran@gen-it.com.ar>
2024-08-27 20:05:16 -0300

    modify dto user

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -14,6 +14,9 @@ export class User {
   @Column({ default: true })
   isActive: boolean;
 
+  @Column({ nullable: false })
+  password: string;
+
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createAt: Date;
 }
