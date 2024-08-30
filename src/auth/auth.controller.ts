95289711393df1336e70636566be2ac1f4218e6d
jduran <jduran@gen-it.com.ar>
2024-08-30 19:22:53 -0300

    implement public and remove guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,31 +2,33 @@ import { Controller, Post, Body, UseGuards, HttpCode, HttpStatus } from '@nestjs
 import { AuthService } from './auth.service';
 import { LoginAuthDto } from './dto/create-auth.dto';
 import { AtGuard, RtGuard } from 'src/commons/guards';
-import { GetCurrentUser, GetCurrentUserId } from 'src/commons/decorators';
+import { GetCurrentUser, GetCurrentUserId, Public } from 'src/commons/decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
+  @Public()
   @Post('login')
   @HttpCode(HttpStatus.CREATED)
   login(@Body() loginAuthDto: LoginAuthDto) {
     return this.authService.login(loginAuthDto);
   }
 
+  @Public()
   @Post('register')
   @HttpCode(HttpStatus.OK)
   register(@Body() loginAuthDto: LoginAuthDto) {
     return this.authService.register(loginAuthDto);
   }
 
-  @UseGuards(AtGuard)
   @Post('logout')
   @HttpCode(HttpStatus.OK)
   logout(@GetCurrentUserId() userId: number) {
     return this.authService.logout(userId);
   }
 
+  @Public()
   @UseGuards(RtGuard)
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
