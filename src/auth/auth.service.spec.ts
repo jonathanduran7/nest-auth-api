8a8748bc0b53accb2562feb0bedca3a6568622b6
jonathanduran7 <joniduran624@gmail.com>
2024-09-01 16:48:31 -0300

    test for register user

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,6 +4,7 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -37,5 +38,46 @@ describe('AuthService', () => {
 
   it('user repository should be defined', () => {
     expect(userRepository).toBeDefined();
+  })
+
+  describe('register', () => {
+    it('should register user', async () => {
+      const loginAuthDto = {
+        email: 'test@example.com',
+        userName: 'testUser',
+        password: 'testPassword',
+      };
+
+      const savedUser: User = {
+        id: 1,
+        email: loginAuthDto.email,
+        userName: loginAuthDto.userName,
+        password: 'hashedPassword',
+        isActive: true,
+        refreshToken: null,
+        createAt: new Date(),
+      };
+
+      const tokens = {
+        access_token: 'access_token_value',
+        refresh_token: 'refresh_token_value',
+      };
+
+      const bcryptHashMock = jest.fn().mockResolvedValue('hashedPassword');
+
+      // mocking methods
+      jest.spyOn(userRepository, 'findOne').mockResolvedValue(null);
+      jest.spyOn(bcrypt, 'hash').mockImplementation(bcryptHashMock);
+      jest.spyOn(userRepository, 'save').mockResolvedValue(savedUser);
+      jest.spyOn(service, 'getTokens').mockResolvedValue(tokens);
+      jest.spyOn(service, 'updateRtHash').mockResolvedValue(undefined);
+
+      const result = await service.register(loginAuthDto);
+      expect(result).toEqual(tokens);
+      expect(userRepository.findOne).toHaveBeenCalledWith({ where: { email: loginAuthDto.email, userName: loginAuthDto.userName } });
+      expect(userRepository.save).toHaveBeenCalledWith({ ...loginAuthDto, password: 'hashedPassword' });
+      expect(service.getTokens).toHaveBeenCalledWith(savedUser.id, savedUser.email);
+      expect(service.updateRtHash).toHaveBeenCalledWith(savedUser.id, tokens.refresh_token);
+    })
   })
 });
